e87bb1bd8b219ae5f477d43aabf9c0f8bdc81ae7
Sam Kao <sam8409044601@gmail.com>
2024-11-28 13:15:41 +0800

    First commit

---
+++ Pthread/CMakeFiles/DisplayImage.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zailair/Desktop/PP_HW/Final_Project_Pthread_Final/BM_pthread_block.cpp" "/home/zailair/Desktop/PP_HW/Final_Project_Pthread_Final/CMakeFiles/DisplayImage.dir/BM_pthread_block.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
